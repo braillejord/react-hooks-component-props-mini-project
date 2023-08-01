c22f1b36ddd7dad8ce7ad3c308d67dcfe07d7552
BreElle Wells <breellejordyn@gmail.com>
2023-08-01 17:25:13 -0600

    complete bonus deliverable

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} • {minutesRead(minutes)} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
 }
 
 export default Article;
+
+
+function minutesRead(minutes) {
+    const coffeeInterval = 5;
+    const bentoInterval = 10;
+    const numCoffees = Math.ceil(minutes / coffeeInterval)
+    const numBentos = Math.ceil(minutes / bentoInterval)
+
+    return minutes <= 30 ? '☕️'.repeat(numCoffees) : '🍱'.repeat(numBentos)
+}
